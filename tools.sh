1000c87444dafe003b9a9963b292a100e9c78024
vinai <vinaibangala2001@gmail.com>
2020-01-12 14:22:35 +0530

    updates/corrections

--- tools.sh
+++ tools.sh
@@ -3,3 +3,7 @@ du -skh
 
 # lauch shell in root, if you don't know root password and you are a sudoer
 sudo bash
+
+# apt search packages: list available packages to download
+apt search libncur*		# shows whether it is [installed] or not
+man apt
